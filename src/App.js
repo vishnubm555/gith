0735742f3728ef7857d42bcaba6200803fb27a08
vishnubm555 <vishnuchinnni555@gmail.com>
2025-02-03 17:15:07 +0530

    added name and USN

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         <h1>Peer-Mentor Learning Series Workshop 1</h1>
         <div className="user-info">
-          <p><strong>Name:</strong> Your Name</p> 
-          <p><strong>USN:</strong> Your USN</p>
+          <p><strong>Name:</strong>Vishnu</p> 
+          <p><strong>USN:</strong>540</p>
         </div>
       </header>
     </div>
